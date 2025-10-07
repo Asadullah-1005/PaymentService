47363c4f8ee0e30bffe409a47951e67aa5f99128
Asadullah-1005 <aullah@tulip-tech.com>
2025-10-07 21:35:39 +0600

    Initial Commit

---
+++ src/test/java/com/Tulip_Tech/PaymentService/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tulip_Tech.PaymentService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
