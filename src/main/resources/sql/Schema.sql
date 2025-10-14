5881b5a9c8ce7a11de82d89671b31a56bd147731
Asadullah-1005 <aullah@tulip-tech.com>
2025-10-14 15:09:48 +0600

    Fix: Sql Syntax error

--- src/main/resources/sql/Schema.sql
+++ src/main/resources/sql/Schema.sql
@@ -1,8 +1,9 @@
+use payment;
 CREATE TABLE PAYMENT(
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     orderId BIGINT NOT NULL ,
     paymentMode VARCHAR(50) NOT NULL ,
-    referenceNumber VARCHAR(100).
+    referenceNumber VARCHAR(100),
     paymentDate TIMESTAMP NULL,
     paymentStatus VARCHAR(50),
     amount BIGINT NOT NULL
