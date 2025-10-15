44d31d2fe9e50590d5a08cf508d0bdc77fa7d28d
Asadullah-1005 <aullah@tulip-tech.com>
2025-10-15 16:07:24 +0600

    Refactor PaymentEntity

--- src/main/java/com/Tulip_Tech/PaymentService/entity/PaymentEntity.java
+++ src/main/java/com/Tulip_Tech/PaymentService/entity/PaymentEntity.java
@@ -17,24 +17,31 @@ public class PaymentEntity {
 
     @Id
     @GeneratedValue
+    @Column(name = "payment_id")
     private Long id;
 
     @Setter
+    @Column(name = "order_id")
     private Long orderId;
 
     @Setter
     @Enumerated(EnumType.STRING)
+    @Column(name = "payment_mode")
     private Payment_Mode paymentMode;
 
     @Setter
+    @Column(name = "reference_number")
     private String referenceNumber;
 
     @Setter
+    @Column(name = "payment_date")
     private Instant paymentDate;
 
     @Setter
+    @Column(name = "payment_status")
     private String paymentStatus;
 
     @Setter
+    @Column(name = "amount")
     private Long amount;
 }
